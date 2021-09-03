a21eb17200e87e81a3726637c24d6488724fb764
Asutosh <asugupta@paypal.com>
2021-09-03 22:32:26 +0530

    Callback function written

--- ESPN_Olympics2020_Scraping/homepage.js
+++ ESPN_Olympics2020_Scraping/homepage.js
@@ -5,4 +5,17 @@ let cheerio=require("cheerio");
 let link="https://www.espncricinfo.com/"
 request(link,cb);
 
-function cb(error,response,html)
+function cb(error,response,html){
+    if(error==null && response.statusCode==200){
+        parseData(html);
+    }
+    else if(response.statusCode==404){
+        console.log("Resours not found");
+    }else{
+        console.log(error);
+    }
+}
+
+function parseData(html){
+    
+}
